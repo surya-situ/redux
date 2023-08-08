de0ea529c9e8fd25ef0bf3f066912657a3269ff1
Suryakanta Das <suryasocial55@gmail.com>
2023-08-08 14:48:11 +0530

    all products added to the home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Product from '../components/Product'
+
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Redux Store:</h1>
+        <Product />
+    </div>
+  )
+}
+
+export default Home
